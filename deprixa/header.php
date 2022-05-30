c09d908040ff7d02061437e9589d0271a2596b2f
fahmi arif <fahmiarif88@gmail.com>
2022-05-30 23:50:15 +0700

    update fix logo

--- deprixa/header.php
+++ deprixa/header.php
@@ -72,7 +72,7 @@ isUser();
                 <!-- LOGO -->
                 <div class="topbar-left">
                     <a href="admin.php" class="logo">
-                        <span><img src="../images/citra-logo.png" width="50%"></span>
+                        <span><img src="../images/Citra-logo.png" width="50%"></span>
                     </a>
                 </div>
                 <!-- End Logo container-->
